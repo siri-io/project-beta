3d800d483ec9ac8f7ddf31df0b25676fa33ee008
Sreeram Padmanabhan <sreeram@jsdrome.com>
2021-02-04 19:25:44 +0530

    username update

--- scripts/release_npm.sh
+++ scripts/release_npm.sh
@@ -1,6 +1,6 @@
 set -e
 git config user.email "action@github.com"
-git config user.name "Bot"
+git config user.name "GitHub Action"
 npm i
 npm run release -- --ci minor
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
